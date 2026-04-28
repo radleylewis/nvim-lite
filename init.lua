beead5a690ca194b4d85294756f91860975a4387
starlord <blitzar90@gmail.com>
2026-04-28 11:06:20 +0300

    Update init.lua
    Use previously defined  function instead of new inline definition

--- init.lua
+++ init.lua
@@ -361,12 +361,7 @@ end
 
 -- Key mappings
 vim.keymap.set("n", "<leader>t", FloatingTerminal, { noremap = true, silent = true, desc = "Toggle floating terminal" })
-vim.keymap.set("t", "<Esc>", function()
-  if terminal_state.is_open then
-    vim.api.nvim_win_close(terminal_state.win, false)
-    terminal_state.is_open = false
-  end
-end, { noremap = true, silent = true, desc = "Close floating terminal from terminal mode" })
+vim.keymap.set("t", "<Esc>", CloseFloatingTerminal, { noremap = true, silent = true, desc = "Close floating terminal from terminal mode" })
 
 
 -- ============================================================================
